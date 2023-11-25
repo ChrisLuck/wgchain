2be5b367de9ee9c03139eb910909a82baa73bbf8
ChrisLuck <c_j_l@web.de>
2023-11-25 14:39:20 +0100

    add DNS handling and generate firewall setup

---
+++ wgchain/fwtemplate.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+#            me       VPN0      VPN1
+# Inner VPN  O-------------------0
+# Outer VPN  O=========0
+#
+# me -> $VPN0 -> $VPN1 -> internet
+
+set -e
+set -o pipefail
+
+
+wg-quick up $VPN0
+wg-quick up $VPN1
+
+
+#disable ipv6
+sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sysctl -w net.ipv6.conf.default.disable_ipv6=1
+sysctl -w net.ipv6.conf.lo.disable_ipv6=1
+
+#allow local network communication outside of tunnel
+iptables -A OUTPUT -o $IFACENAME -d $LOCALNET -j ACCEPT
+
+iptables -A OUTPUT -o $IFACENAME -p udp ! --dport $VPN0_EP_PORT -j REJECT
+iptables -A OUTPUT -o $IFACENAME ! -d $VPN0_EP_IP -j REJECT
+
+iptables -A OUTPUT -o $VPN0 -p udp ! --dport $VPN1_EP_PORT -j REJECT
+iptables -A OUTPUT -o $VPN0 ! -d $VPN1_EP_IP -j REJECT
--- wgchain/wgchain.py
+++ wgchain/wgchain.py
@@ -1,4 +1,6 @@
 import argparse
+import subprocess
+from string import Template
 from wgconfig import WgConfig
 from wgconfig import IPList
 from wgconfig import IPWithMask
@@ -19,9 +21,9 @@ cfgs = list(map(WgConfig, args.configs))
 
 
 #modify configs so that:
-# - they all have the same FwMark
+# - They all have the same FwMark
 # - AllowedIPs of vpn #n is set to the endpoint of vpn #n+1
-# - DNS?
+# - DNS addresses af all configs removed except from last/innermost one
 before: WgConfig | None = None
 for cfg in cfgs:
     cfg.interface.fwmark = COMMON_FWMARK
@@ -30,9 +32,42 @@ for cfg in cfgs:
         continue
     before.peer.allowedips = IPList([IPWithMask(str(cfg.peer.endpoint.ip) + "/32")])
 
+    # remove DNS from every config except last/innermost one
+    del before.interface.dns
+
+
+# Sanity checks for DNS fields
+for cfg in cfgs[:-1]:
+    assert not hasattr(cfg.interface, "dns"), "Code for DNS removal is faulty"
+assert hasattr(cfgs[-1].interface, "dns"), "Risk of leakage: Innermost config has no DNS field."
+
+
 for cfg in cfgs:
     print("------")
     print(cfg.ifname)
     __import__('pprint').pprint(cfg.interface)
     __import__('pprint').pprint(cfg.peer)
     cfg.write(cfg.ifname + ".conf")
+
+
+if len(cfgs) == 2:
+    ifacename = subprocess.check_output("ip -o -f inet addr show | awk '/scope global/ {print $2}'", shell=True)
+    ifacename = ifacename.decode().strip()
+    localnet  = subprocess.check_output("ip -o -f inet addr show | awk '/scope global/ {print $4}'", shell=True)
+    localnet  = localnet.decode().strip()
+
+
+    template = None
+    with open("wgchain/fwtemplate.sh", "r") as f:
+        template = Template(f.read())
+
+    with open("fwon.sh", "w") as outfile:
+        mapping = {"IFACENAME":ifacename,
+                   "LOCALNET":localnet,
+                   "VPN0":cfgs[0].ifname,
+                   "VPN1":cfgs[1].ifname,
+                   "VPN0_EP_IP":cfgs[0].peer.endpoint.ip,
+                   "VPN0_EP_PORT":cfgs[0].peer.endpoint.port,
+                   "VPN1_EP_IP":cfgs[1].peer.endpoint.ip,
+                   "VPN1_EP_PORT":cfgs[1].peer.endpoint.port}
+        outfile.write(template.substitute(mapping))
